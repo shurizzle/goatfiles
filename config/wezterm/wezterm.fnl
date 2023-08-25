580314f3c5d3419a2c5e2a763bac52223bc79a03
shurizzle <shura1991@gmail.com>
2023-08-25 12:42:24 +0200

    wezterm: new fennel config

---
+++ config/wezterm/wezterm.fnl
@@ -0,0 +1,196 @@
+(local wezterm (require :wezterm))
+(local hostname (wezterm.hostname))
+(local (arc os) (let [hostname (wezterm.hostname)
+                      i1 (string.find wezterm.target_triple :-)
+                      arch (string.sub wezterm.target_triple 1 (- i1 1))
+                      i2 (string.find wezterm.target_triple :- (+ i1 1))
+                      i3 (string.find wezterm.target_triple :- (+ i2 1))
+                      os (if i3
+                             (string.sub wezterm.target_triple (+ i2 1) (- i3 1))
+                             (string.sub wezterm.target_triple (+ i2 1)))]
+                  (values arch os)))
+
+(set wezterm.GLOBAL.bells {})
+(local *bell* "🔔")
+
+(fn tab-title [tab max-width]
+  (wezterm.truncate_right
+    (.. (if (= (. wezterm.GLOBAL.bells (tostring tab.tab_id)) tab.tab_id) *bell* "")
+        (string.gsub tab.active_pane.title "(.*: )(.*)" "%2"))
+    (- max-width 2)))
+
+(wezterm.on
+  :format-tab-title
+  (fn [tab _ _ _ _ max-width]
+    (tset wezterm.GLOBAL.bells
+          (tostring
+            (: (: (wezterm.mux.get_window tab.window_id) :active_tab) :tab_id))
+          nil)
+    (.. " " (tab-title tab max-width) " ")))
+
+(wezterm.on :bell (fn [window pane]
+                    (let [tab (: (pane:tab) :tab_id)]
+                      (when (not= (: (window:active_tab) :tab_id) tab)
+                        (tset wezterm.GLOBAL.bells (tostring tab) tab)))))
+
+(var config (if wezterm.config_builder (wezterm.config_builder) {}))
+
+(set config.term "xterm-kitty")
+
+(fn ssh-domains []
+  (let [ds (wezterm.default_ssh_domains)]
+    (each [_ dom (ipairs ds)]
+      (set dom.assume_shell :Posix))
+    (each [_ name (ipairs [:vercingetorige :DomPerignon :filottete])]
+      (when (not= name hostname)
+        (table.insert ds {:name name
+                          :remote_address (.. name :.local)
+                          :multiplexing (if (= name :filottete) :None :WezTerm)
+                          :assume_shell :Posix})))
+    ds))
+(set config.ssh_domains (ssh-domains))
+
+(set config.mux_env_remove {})
+
+(set config.front_end :WebGpu)
+
+(set config.window_decorations (if (= os :linux) :NONE :RESIZE))
+(set config.window_padding {:left 0
+                            :right 0
+                            :top 0
+                            :bottom 0})
+(set config.use_ime true)
+
+(set config.enable_tab_bar true)
+(set config.use_fancy_tab_bar false)
+(set config.hide_tab_bar_if_only_one_tab true)
+(set config.show_new_tab_button_in_tab_bar false)
+(set config.show_tab_index_in_tab_bar false)
+(set config.tab_bar_at_bottom true)
+
+(set config.audible_bell :SystemBeep)
+(set config.enable_wayland true)
+
+(set config.allow_square_glyphs_to_overflow_width :Always)
+(set config.harfbuzz_features [:ss01
+                               :ss02
+                               :ss03
+                               :ss04
+                               :ss05
+                               :ss06
+                               :zero
+                               :onum])
+(set config.font (wezterm.font_with_fallback ["CommitMono"
+                                              "Hack Nerd Font Mono"
+                                              "JetBrains Mono"
+                                              "Noto Color Emoji"
+                                              "Symbols Nerd Font Mono"]))
+(set config.font_size (if (= os :linux) 10 11))
+
+(set config.default_cursor_style :SteadyBar)
+
+; (set config.enable_kitty_keyboard true)
+(set config.enable_csi_u_key_encoding true)
+
+(set config.colors {:background    :#282828
+                    :foreground    :#eeeeee
+
+                    :cursor_bg     :#ffffff
+                    :cursor_fg     :#000000
+
+                    :cursor_border :#ffffff
+
+                    :ansi          [:#282828
+                                    :#c8213d
+                                    :#169C51
+                                    :#DAAF19
+                                    :#2F90FE
+                                    :#C14ABE
+                                    :#48C6DB
+                                    :#CBCBCB]
+                    :brights       [:#505050
+                                    :#C7213D
+                                    :#1ef15f
+                                    :#FFE300
+                                    :#00aeff
+                                    :#FF40BE
+                                    :#48FFFF
+                                    :#ffffff]})
+
+(local cpmods (if (= os :darwin) :CMD :CTRL|SHIFT))
+(set config.disable_default_key_bindings true)
+(set config.leader {:key :a :mods :CTRL :timeout_milliseconds 1000})
+
+(fn common-keys []
+  [{:key    :q
+    :mods   :LEADER
+    :action (wezterm.action.CloseCurrentPane {:confirm true})}
+   {:key    :c
+    :mods   :LEADER
+    :action (wezterm.action.SpawnTab :CurrentPaneDomain)}
+   {:key    :h
+    :mods   :CTRL|LEADER
+    :action (wezterm.action.ActivateTabRelative -1)}
+    {:key    :l
+     :mods   :CTRL|LEADER
+     :action (wezterm.action.ActivateTabRelative 1)}
+   {:key    :h
+    :mods   :LEADER
+    :action (wezterm.action.ActivatePaneDirection :Left)}
+   {:key    :j
+    :mods   :LEADER
+    :action (wezterm.action.ActivatePaneDirection :Down)}
+   {:key    :k
+    :mods   :LEADER
+    :action (wezterm.action.ActivatePaneDirection :Up)}
+   {:key    :l
+    :mods   :LEADER
+    :action (wezterm.action.ActivatePaneDirection :Right)}
+   {:key    :|
+    :mods   :LEADER
+    :action (wezterm.action.SplitHorizontal {:domain :CurrentPaneDomain})}
+   {:key    :|
+    :mods   :SHIFT|LEADER
+    :action (wezterm.action.SplitHorizontal {:domain :CurrentPaneDomain})}
+   {:key    :-
+    :mods   :LEADER
+    :action (wezterm.action.SplitVertical {:domain :CurrentPaneDomain})}
+   {:key    " "
+    :mods   :LEADER
+    :action wezterm.action.ShowLauncher}
+   {:key    :a
+    :mods   :LEADER|CTRL
+    :action (wezterm.action.SendString "\x01")}
+   {:key    ::
+    :mods   :SHIFT|LEADER
+    :action wezterm.action.ShowDebugOverlay}
+   {:key    ::
+    :mods   :LEADER
+    :action wezterm.action.ShowDebugOverlay}
+   {:key    :v
+    :mods   cpmods
+    :action (wezterm.action.PasteFrom :Clipboard)}
+   {:key    :c
+    :mods   cpmods
+    :action (wezterm.action.CopyTo :Clipboard)}])
+
+(fn macos-keys []
+  (if (= os :darwin)
+      [{:key    ","
+        :mods   :CTRL
+        :action (wezterm.action.SendString "\27[44;5u")}
+       {:key    ","
+        :mods   :CTRL|SHIFT
+        :action (wezterm.action.SendString "\27[44;6u")}]
+      []))
+
+(set config.keys (let [keys (common-keys)]
+                   (each [_ k (ipairs (macos-keys))]
+                     (table.insert keys k))
+                   keys))
+
+(set config.mouse_bindings [{:event {:Up {:streak 1 :button :Left}}
+                             :mods :CTRL
+                             :action wezterm.action.OpenLinkAtMouseCursor}])
+
+config
--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,280 +1,126 @@
 local wezterm = require("wezterm")
 local hostname = wezterm.hostname()
-
-local arch, os_name = (function()
-	local i1 = string.find(wezterm.target_triple, "-")
-	local arch = string.sub(wezterm.target_triple, 1, i1 - 1)
-	local i2 = string.find(wezterm.target_triple, "-", i1 + 1)
-	local i3 = string.find(wezterm.target_triple, "-", i2 + 1)
-	local os
-	if i3 then
-		os = string.sub(wezterm.target_triple, i2 + 1, i3 - 1)
-	else
-		os = string.sub(wezterm.target_triple, i2 + 1)
-	end
-
-	return arch, os
-end)()
-_ = arch
-_ = os_name
-
+local arc, os = nil, nil
+do
+  local hostname0 = wezterm.hostname()
+  local i1 = string.find(wezterm.target_triple, "-")
+  local arch = string.sub(wezterm.target_triple, 1, (i1 - 1))
+  local i2 = string.find(wezterm.target_triple, "-", (i1 + 1))
+  local i3 = string.find(wezterm.target_triple, "-", (i2 + 1))
+  local os0
+  if i3 then
+    os0 = string.sub(wezterm.target_triple, (i2 + 1), (i3 - 1))
+  else
+    os0 = string.sub(wezterm.target_triple, (i2 + 1))
+  end
+  arc, os = arch, os0
+end
 wezterm.GLOBAL.bells = {}
-
-local BELL = "🔔"
-
+local _2abell_2a = "\240\159\148\148"
 local function tab_title(tab, max_width)
-	local bell = wezterm.GLOBAL.bells[tostring(tab.tab_id)] == tab.tab_id and BELL or ""
-	return wezterm.truncate_right(bell .. string.gsub(tab.active_pane.title, "(.*: )(.*)", "%2"), max_width - 2)
+  local function _2_()
+    if (wezterm.GLOBAL.bells[tostring(tab.tab_id)] == tab.tab_id) then
+      return _2abell_2a
+    else
+      return ""
+    end
+  end
+  return wezterm.truncate_right((_2_() .. string.gsub(tab.active_pane.title, "(.*: )(.*)", "%2")), (max_width - 2))
 end
-
-wezterm.on("format-tab-title", function(
-	tab,
-	_, --[[ tabs ]]
-	_, --[[ panes ]]
-	_, --[[ config ]]
-	_, --[[ hover ]]
-	max_width
-)
-	wezterm.GLOBAL.bells[tostring(wezterm.mux.get_window(tab.window_id):active_tab():tab_id())] = nil
-	return " " .. tab_title(tab, max_width) .. " "
-end)
-
-wezterm.on("bell", function(window, pane)
-	local tab = pane:tab():tab_id()
-	if window:active_tab():tab_id() ~= tab then
-		wezterm.GLOBAL.bells[tostring(tab)] = tab
-	end
-end)
-
-local config = {}
+local function _3_(tab, _, _0, _1, _2, max_width)
+  wezterm.GLOBAL.bells[tostring(wezterm.mux.get_window(tab.window_id):active_tab():tab_id())] = nil
+  return (" " .. tab_title(tab, max_width) .. " ")
+end
+wezterm.on("format-tab-title", _3_)
+local function _4_(window, pane)
+  local tab = pane:tab():tab_id()
+  if (window:active_tab():tab_id() ~= tab) then
+    wezterm.GLOBAL.bells[tostring(tab)] = tab
+    return nil
+  else
+    return nil
+  end
+end
+wezterm.on("bell", _4_)
+local config
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
+else
+  config = {}
 end
-
 config.term = "xterm-kitty"
-
-config.ssh_domains = wezterm.default_ssh_domains()
-for _, dom in ipairs(config.ssh_domains) do
-	dom.assume_shell = "Posix"
+local function ssh_domains()
+  local ds = wezterm.default_ssh_domains()
+  for _, dom in ipairs(ds) do
+    dom.assume_shell = "Posix"
+  end
+  for _, name in ipairs({"vercingetorige", "DomPerignon", "filottete"}) do
+    if (name ~= hostname) then
+      local _7_
+      if (name == "filottete") then
+        _7_ = "None"
+      else
+        _7_ = "WezTerm"
+      end
+      table.insert(ds, {name = name, remote_address = (name .. ".local"), multiplexing = _7_, assume_shell = "Posix"})
+    else
+    end
+  end
+  return ds
 end
-
-if hostname ~= "vercingetorige" then
-	table.insert(config.ssh_domains, {
-		name = "vercingetorige",
-		remote_address = "vercingetorige.local",
-		multiplexing = "WezTerm",
-		assume_shell = "Posix",
-	})
-end
-
-if hostname ~= "DomPerignon" then
-	table.insert(config.ssh_domains, {
-		name = "DomPerignon",
-		remote_address = "DomPerignon.local",
-		multiplexing = "WezTerm",
-		assume_shell = "Posix",
-	})
-end
-
-if hostname ~= "filottete" then
-	table.insert(config.ssh_domains, {
-		name = "filottete",
-		remote_address = "filottete.local",
-		multiplexing = "None",
-		assume_shell = "Posix",
-	})
-end
-
+config.ssh_domains = ssh_domains()
 config.mux_env_remove = {}
-
 config.front_end = "WebGpu"
-
-config.window_decorations = os_name == "linux" and "NONE" or "RESIZE"
-config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
-}
+if (os == "linux") then
+  config.window_decorations = "NONE"
+else
+  config.window_decorations = "RESIZE"
+end
+config.window_padding = {left = 0, right = 0, top = 0, bottom = 0}
 config.use_ime = true
-
 config.enable_tab_bar = true
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.show_new_tab_button_in_tab_bar = false
 config.show_tab_index_in_tab_bar = false
 config.tab_bar_at_bottom = true
-
 config.audible_bell = "SystemBeep"
 config.enable_wayland = true
-
 config.allow_square_glyphs_to_overflow_width = "Always"
-config.harfbuzz_features = {
-	"ss01",
-	"ss02",
-	"ss03",
-	"ss04",
-	"ss05",
-	"ss06",
-	"zero",
-	"onum",
-}
-config.font = wezterm.font_with_fallback({
-	"CommitMono",
-	"Hack Nerd Font Mono",
-	"JetBrains Mono",
-	"Noto Color Emoji",
-	"Symbols Nerd Font Mono",
-})
-config.font_size = os_name == "linux" and 10 or 11
-
-config.default_cursor_style = "SteadyBar"
-
--- config.enable_kitty_keyboard = true
-config.enable_csi_u_key_encoding = true
-
-config.colors = {
-	background = "#282828",
-	foreground = "#eeeeee",
-
-	cursor_bg = "#ffffff",
-	cursor_fg = "#000000",
-
-	cursor_border = "#ffffff",
-
-	ansi = {
-		"#282828",
-		"#c8213d",
-		"#169C51",
-		"#DAAF19",
-		"#2F90FE",
-		"#C14ABE",
-		"#48C6DB",
-		"#CBCBCB",
-	},
-	brights = {
-		"#505050",
-		"#C7213D",
-		"#1ef15f",
-		"#FFE300",
-		"#00aeff",
-		"#FF40BE",
-		"#48FFFF",
-		"#ffffff",
-	},
-}
-
-local cpmods = os_name == "darwin" and "CMD" or "CTRL|SHIFT"
-
-config.disable_default_key_bindings = true
-config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
-local keys = {
-	{
-		key = "q",
-		mods = "LEADER",
-		action = wezterm.action.CloseCurrentPane({ confirm = true }),
-	},
-	{
-		key = "c",
-		mods = "LEADER",
-		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
-	},
-	{
-		key = "h",
-		mods = "CTRL|LEADER",
-		action = wezterm.action.ActivateTabRelative(-1),
-	},
-	{
-		key = "l",
-		mods = "CTRL|LEADER",
-		action = wezterm.action.ActivateTabRelative(1),
-	},
-	{
-		key = "h",
-		mods = "LEADER",
-		action = wezterm.action.ActivatePaneDirection("Left"),
-	},
-	{
-		key = "j",
-		mods = "LEADER",
-		action = wezterm.action.ActivatePaneDirection("Down"),
-	},
-	{
-		key = "k",
-		mods = "LEADER",
-		action = wezterm.action.ActivatePaneDirection("Up"),
-	},
-	{
-		key = "l",
-		mods = "LEADER",
-		action = wezterm.action.ActivatePaneDirection("Right"),
-	},
-	{
-		key = "|",
-		mods = "LEADER",
-		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		key = "|",
-		mods = "SHIFT|LEADER",
-		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		key = "-",
-		mods = "LEADER",
-		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		key = " ",
-		mods = "LEADER",
-		action = wezterm.action.ShowLauncher,
-	},
-	{
-		key = "a",
-		mods = "LEADER|CTRL",
-		action = wezterm.action.SendString("\x01"),
-	},
-	{
-		key = ":",
-		mods = "SHIFT|LEADER",
-		action = wezterm.action.ShowDebugOverlay,
-	},
-	{
-		key = ":",
-		mods = "LEADER",
-		action = wezterm.action.ShowDebugOverlay,
-	},
-	{
-		key = "v",
-		mods = cpmods,
-		action = wezterm.action.PasteFrom("Clipboard"),
-	},
-	{
-		key = "c",
-		mods = cpmods,
-		action = wezterm.action.CopyTo("Clipboard"),
-	},
-}
-if os_name == "darwin" then
-	table.insert(keys, {
-		key = ",",
-		mods = "CTRL",
-		action = wezterm.action.SendString("\27[44;5u"),
-	})
-	table.insert(keys, {
-		key = ",",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.SendString("\27[44;6u"),
-	})
+config.harfbuzz_features = {"ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "zero", "onum"}
+config.font = wezterm.font_with_fallback({"CommitMono", "Hack Nerd Font Mono", "JetBrains Mono", "Noto Color Emoji", "Symbols Nerd Font Mono"})
+if (os == "linux") then
+  config.font_size = 10
+else
+  config.font_size = 11
 end
-config.keys = keys
-
-config.mouse_bindings = {
-	{
-		event = { Up = { streak = 1, button = "Left" } },
-		mods = "CTRL",
-		action = wezterm.action.OpenLinkAtMouseCursor,
-	},
-}
-
+config.default_cursor_style = "SteadyBar"
+config.enable_csi_u_key_encoding = true
+config.colors = {background = "#282828", foreground = "#eeeeee", cursor_bg = "#ffffff", cursor_fg = "#000000", cursor_border = "#ffffff", ansi = {"#282828", "#c8213d", "#169C51", "#DAAF19", "#2F90FE", "#C14ABE", "#48C6DB", "#CBCBCB"}, brights = {"#505050", "#C7213D", "#1ef15f", "#FFE300", "#00aeff", "#FF40BE", "#48FFFF", "#ffffff"}}
+local cpmods
+if (os == "darwin") then
+  cpmods = "CMD"
+else
+  cpmods = "CTRL|SHIFT"
+end
+config.disable_default_key_bindings = true
+config.leader = {key = "a", mods = "CTRL", timeout_milliseconds = 1000}
+local function common_keys()
+  return {{key = "q", mods = "LEADER", action = wezterm.action.CloseCurrentPane({confirm = true})}, {key = "c", mods = "LEADER", action = wezterm.action.SpawnTab("CurrentPaneDomain")}, {key = "h", mods = "CTRL|LEADER", action = wezterm.action.ActivateTabRelative(-1)}, {key = "l", mods = "CTRL|LEADER", action = wezterm.action.ActivateTabRelative(1)}, {key = "h", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Left")}, {key = "j", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Down")}, {key = "k", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Up")}, {key = "l", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Right")}, {key = "|", mods = "LEADER", action = wezterm.action.SplitHorizontal({domain = "CurrentPaneDomain"})}, {key = "|", mods = "SHIFT|LEADER", action = wezterm.action.SplitHorizontal({domain = "CurrentPaneDomain"})}, {key = "-", mods = "LEADER", action = wezterm.action.SplitVertical({domain = "CurrentPaneDomain"})}, {key = " ", mods = "LEADER", action = wezterm.action.ShowLauncher}, {key = "a", mods = "LEADER|CTRL", action = wezterm.action.SendString("\1")}, {key = ":", mods = "SHIFT|LEADER", action = wezterm.action.ShowDebugOverlay}, {key = ":", mods = "LEADER", action = wezterm.action.ShowDebugOverlay}, {key = "v", mods = cpmods, action = wezterm.action.PasteFrom("Clipboard")}, {key = "c", mods = cpmods, action = wezterm.action.CopyTo("Clipboard")}}
+end
+local function macos_keys()
+  if (os == "darwin") then
+    return {{key = ",", mods = "CTRL", action = wezterm.action.SendString("\27[44;5u")}, {key = ",", mods = "CTRL|SHIFT", action = wezterm.action.SendString("\27[44;6u")}}
+  else
+    return {}
+  end
+end
+do
+  local keys = common_keys()
+  for _, k in ipairs(macos_keys()) do
+    table.insert(keys, k)
+  end
+  config.keys = keys
+end
+config.mouse_bindings = {{event = {Up = {streak = 1, button = "Left"}}, mods = "CTRL", action = wezterm.action.OpenLinkAtMouseCursor}}
 return config
