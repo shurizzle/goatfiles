de43caf3c557ab0d5aebe5b743a9b96d5af59347
shurizzle <shura1991@gmail.com>
2023-08-26 01:20:59 +0200

    fix: remove luv support from fennel.mk

--- fennel.mk
+++ fennel.mk
@@ -1,37 +1,37 @@
 GOATFILES_ROOT := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 BUNDLED_FENNEL_BIN := $(GOATFILES_ROOT)fennel
 
-ifeq (, $(FENNELUV))
-$(shell fennel -e "(require :luv)" 2>/dev/null >/dev/null)
-ifeq (0, $(.SHELLSTATUS))
-FENNELUV := $(shell which fennel)
-endif
-endif
-
-ifeq (, $(LUAUV))
-$(shell luajit -e "require'luv'" 2>/dev/null >/dev/null)
-ifeq (0, $(.SHELLSTATUS))
-LUAUV := $(shell which luajit)
-endif
-endif
-
-ifeq (, $(LUAUV))
-$(shell lua -e "require'luv'" 2>/dev/null >/dev/null)
-ifeq (0, $(.SHELLSTATUS))
-LUAUV := $(shell which lua)
-endif
-endif
-
-ifeq (, $(LUAUV))
-LUAUV := $(shell which nvim) -u NONE --headless -l $(BUNDLED_FENNEL_BIN)
-ifeq (0, $(.SHELLSTATUS))
-$(error "No fennel/lua/luajit with luv or neovim in PATH")
-endif
-endif
-
-ifeq (, $(FENNELUV))
-FENNELUV := $(LUAUV) $(BUNDLED_FENNEL_BIN)
-endif
+# ifeq (, $(FENNELUV))
+# $(shell fennel -e "(require :luv)" 2>/dev/null >/dev/null)
+# ifeq (0, $(.SHELLSTATUS))
+# FENNELUV := $(shell which fennel)
+# endif
+# endif
+#
+# ifeq (, $(LUAUV))
+# $(shell luajit -e "require'luv'" 2>/dev/null >/dev/null)
+# ifeq (0, $(.SHELLSTATUS))
+# LUAUV := $(shell which luajit)
+# endif
+# endif
+#
+# ifeq (, $(LUAUV))
+# $(shell lua -e "require'luv'" 2>/dev/null >/dev/null)
+# ifeq (0, $(.SHELLSTATUS))
+# LUAUV := $(shell which lua)
+# endif
+# endif
+#
+# ifeq (, $(LUAUV))
+# LUAUV := $(shell which nvim) -u NONE --headless -l $(BUNDLED_FENNEL_BIN)
+# ifeq (0, $(.SHELLSTATUS))
+# $(error "No fennel/lua/luajit with luv or neovim in PATH")
+# endif
+# endif
+#
+# ifeq (, $(FENNELUV))
+# FENNELUV := $(LUAUV) $(BUNDLED_FENNEL_BIN)
+# endif
 
 ifeq (, $(FENNEL))
 FENNEL := $(shell which fennel)
