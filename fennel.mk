21e77baed759cda899fbc8288be53d24500ae7fb
shurizzle <me@shurizzle.dev>
2023-08-26 00:16:10 +0200

    fix: correct lua interpreter discovery

--- fennel.mk
+++ fennel.mk
@@ -5,7 +5,7 @@ endif
 ifeq (, $(FENNEL))
 ifneq (, $(shell which luajit))
 FENNEL := luajit $(dir $(abspath $(lastword $(MAKEFILE_LIST))))fennel
-else ifeq (, $(shell which lua))
+else ifneq (, $(shell which lua))
 FENNEL := lua $(dir $(abspath $(lastword $(MAKEFILE_LIST))))fennel
 else
 $(error "No fennel or lua/luajit in PATH")
